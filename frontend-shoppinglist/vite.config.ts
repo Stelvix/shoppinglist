c1c3195da46828570b427cfe99c438bba8aa72a0
Steeven HOUNKPE-SAGBO <e.hounkpesagbo@ecoles-epsi.net>
2026-07-28 19:34:43 +0200

    mise en place de la PWA

--- frontend-shoppinglist/vite.config.ts
+++ frontend-shoppinglist/vite.config.ts
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
  
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+
+ VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "ShoppingList",
+        short_name: "ShoppingList",
+        description: "Application de gestion de courses",
+        theme_color: "#2563eb",
+        background_color: "#f8fafc",
+        display: "standalone",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/frontend-shoppinglist/public/LogoShoppingList.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/frontend-shoppinglist/public/LogoShoppingList.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
    ],
 })
